65b0b3a6081508a4c908a2a21a6cf3877b600b3b
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-01-22 01:29:43 +0900

    Create Main_1152.java

---
+++ LSH/220123/문자열/Main_1152.java
@@ -0,0 +1,14 @@
+// BOJ - 1152(단어의 개수)
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		
+		StringTokenizer st = new StringTokenizer(s, " ");
+		System.out.println(st.countTokens());
+	}
+}
