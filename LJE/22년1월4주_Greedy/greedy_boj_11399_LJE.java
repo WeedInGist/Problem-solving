e9830b8fc77e094884b47a3cb913ae0f922dab32
jieun <lgeun123@naver.com>
2022-01-26 23:39:06 +0900

    22년1월4주차/greedy_boj_1263_LJE

--- LJE/22년1월4주_Greedy/greedy_boj_11399_LJE.java
+++ LJE/22년1월4주_Greedy/greedy_boj_11399_LJE.java
@@ -3,7 +3,7 @@ package greedy;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class BJ_11399 {
+public class greedy_boj_11399_LJE {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,7 +15,7 @@ public class BJ_11399 {
 			time[i] = sc.nextInt();
 		}
 		//------입력 끝
-
+		
 		Arrays.sort(time); // 정렬
 		for(int i=0;i<n;i++) { //배열 크기만큼
 			for(int j=0;j<=i;j++) {
@@ -25,3 +25,4 @@ public class BJ_11399 {
 		System.out.println(sum);
 	}
 }
+
