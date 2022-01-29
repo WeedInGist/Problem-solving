6e5b20a5150ed1cc6b06e1384bd6428e0eaec113
WeedInGist <90121415+WeedInGist@users.noreply.github.com>
2022-01-30 02:45:11 +0900

    탐욕법-백준-1263번-조성민

---
+++ JSM/Boj_1263.java
@@ -0,0 +1,56 @@
+package Algo_study.Greedy;
+
+import java.util.Scanner;
+import java.util.Arrays;
+//커밋 메시지 추가용
+//쉽게 정렬을 해주기 위해 스케쥴 클래스를 만든 뒤 Comparable 인터페이스를 확장함.
+class Schedule implements Comparable<Schedule>
+{
+    int needed_time = 0;
+    int deadline = 0;
+    //정렬을 위한 compareTo 오버라이드 부분.
+    public int compareTo(Schedule a)
+    {
+        return this.deadline-a.deadline;
+    }
+
+}
+class BOJ_1263{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int Task = sc.nextInt();
+        Schedule[] arr = new Schedule[Task];
+        for(int i = 0 ; i < Task; i++)
+        {
+            arr[i] = new Schedule();
+            arr[i].needed_time = sc.nextInt();
+            arr[i].deadline = sc.nextInt();
+        }
+        //0시부터 시작을 할 수 있다고 칠 때, 3 5 , 5 9 -> 적어도 4시에는 시작을 해야 됨. 4시에서 3시간을 뺀, 1시에는 늦어도 시작을 해야된다.
+        // 3 5, 5 9 , 3 11 -> 늦어도 8시에는 시작을 해야되요. 8시에서 5시간을 뺀 늦어도 3시에는 시작
+        // 3시에 시작 0시에 시작해줘야 함.   
+        //스케쥴 인스턴스에 정보들을 받고 완수해야되는 시간 기준으로 정렬함. 빠를수록 앞에 있음.
+        Arrays.sort(arr);
+        //기준점을 잡아주기 위해 포문 밖에 걍 초기화함.
+        int time = arr[Task-1].deadline-arr[Task-1].needed_time;
+        //자세한 부분은 스터디 때 설명해야 할 듯.
+        //스케쥴을 한 개, 두 개, 세 개 늘려가며 생각해보면 쉬움.
+        for(int i = Task -2 ; i >= 0 ; i--)
+        {
+            if(arr[i].deadline < time)
+            {
+                time = arr[i].deadline;
+                time -= arr[i].needed_time;
+            }
+            else
+            {
+                time -= arr[i].needed_time;
+            }
+//            System.out.println(time);
+        }
+        System.out.println(time);
+        
+    
+    }
+}
