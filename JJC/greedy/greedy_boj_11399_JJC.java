216c3d5530803300330e931822b978a09998c933
cheolup <97794900+cheolup@users.noreply.github.com>
2022-01-29 19:17:35 +0900

    greedy_boj_JJC

--- JJC/greedy/greedy_boj_11399_JJC.java
+++ JJC/greedy/greedy_boj_11399_JJC.java
@@ -12,12 +12,13 @@ public class greedy_boj_11399_JJC {
 		for(int i=0;i<n;i++){
 			line[i]=sc.nextInt();
 		}
-		Arrays.sort(line);
-		int result=0;
+
+		Arrays.sort(line); // 시간의 합을 최소하기 위해 시간이 가장 적은 순으로 줄을 세운다 
+		int result=0; 
 		int sum=0;
 		for(int i=0;i<n;i++){
-			sum+=line[i];
-			result+=sum;
+			sum+=line[i]; // 각 사람이 인출하기까지 시간은 앞사람이 걸린 시간에 자신의 필요한 시간을 더한 값이다.
+			result+=sum; // 각 사람의 인출 시간을 더한다.
 		}
 		System.out.println(result);
 
--- JJC/greedy/greedy_boj_1263_JJC.java
+++ JJC/greedy/greedy_boj_1263_JJC.java
@@ -14,7 +14,8 @@ public class greedy_boj_1263_JJC {
 			ti[i] = sc.nextInt();
 			si[i] = sc.nextInt();
 		}
-		for (int i = 0; i < n; i++) {
+
+		for (int i = 0; i < n; i++) { // 배열 si의 내림차순 순서에 맞게 배열 ti도 정렬한다.
 			for (int j = i + 1; j < n; j++) {
 				if (si[i] < si[j]) {
 					int tmp = si[i];
@@ -26,7 +27,8 @@ public class greedy_boj_1263_JJC {
 				}
 			}
 		}
-		int dif = si[0] - ti[0];
+
+		int dif = si[0] - ti[0]; //마감시간에서 걸리는 시간의 차이
 		for (int i = 1; i < n; i++) {
 			if (dif > si[i]) {
 				dif = si[i] - ti[i];
@@ -35,7 +37,8 @@ public class greedy_boj_1263_JJC {
 			}
 
 		}
-		if (dif > 0) {
+
+		if (dif > 0) { // 최종 dif 값이 0보다 크다면 0시부터 시작해서 일을 끝마칠 수 있다.
 			System.out.println(dif);
 		} else {
 			System.out.println("-1");
