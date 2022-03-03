5d1f2484866f5044bd5501947f30f5c23a363835
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-03-04 00:34:12 +0900

    BOJ - 2xn 타일링(11726번) - DP

---
+++ LSH/DP/DP_11726_LSH.java
@@ -0,0 +1,19 @@
+// BOJ - 2xn 타일링(11726번)
+// DP
+
+import java.util.Scanner;
+
+public class DP_11726_LSH {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = (dp[i-1]+dp[i-2]) % 10007;
+
+        }
+        System.out.println(dp[n]);
+    }
+}
